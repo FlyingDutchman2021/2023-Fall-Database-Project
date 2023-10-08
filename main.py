98823f9ac68b362c9362eb7ee5f74ae9a762fc40
FlyingDutchman2021 <89907122+FlyingDutchman2021@users.noreply.github.com>
2023-10-08 16:22:09 +0800

    function setup_main_window

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import tkinter as tk
 
 
 class MainWindow:
-    def __init__(self):
-        self.window = tk.Tk()
+    def setup_main_window(self):
+
         full_width = self.window.winfo_screenwidth()
         full_height = self.window.winfo_screenheight()
         width_percentage = 0.82
@@ -12,10 +12,16 @@ class MainWindow:
             '%dx%d+%d+%d' % (full_width * width_percentage, full_height * height_percentage,
                              full_width * (1 - width_percentage) / 2,
                              full_height * ((1 - height_percentage) / 2 - 0.015)))
-        self.window.title('This is a test')
+        self.window.title('This Is A Test')
+
+    def __init__(self):
+        self.window = tk.Tk()
+        self.setup_main_window()
+
+
 
         # Main loop
         self.window.mainloop()
-
-
+    
+    
 main_window = MainWindow()
