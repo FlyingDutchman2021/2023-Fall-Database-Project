75be591f487d9a766c1066f5b2ed85d9e7c2a5e5
yangjun <1970520843@qq.com>
2023-10-12 21:51:24 +0800

    1

--- frame.py
+++ frame.py
@@ -6,13 +6,34 @@ class Base_Frame:
         self.tk_frame = tk.Frame(master)
 
 
-class Log_In_Frame(Base_Frame):
+class Log_In_frame(Base_Frame):
     def __init__(self, master):
         super().__init__(master)
-        self.line1 = tk.StringVar()
-        self.line2 = tk.StringVar()
-        self.line3 = tk.StringVar()
+        self.account = tk.StringVar()
+        self.password = tk.StringVar()
+        self.frame1 = tk.Frame(master)
+        self.frame2 = tk.Frame(master)
+        self.frame3 = tk.Frame(master)
 
-        tk.Entry(master=self.tk_frame, textvariable=self.line1).pack(side="left")
-        tk.Entry(master=self.tk_frame, textvariable=self.line2).pack(side="left")
-        tk.Entry(master=self.tk_frame, textvariable=self.line3).pack(side="left")
+        tk.Label(master=self.frame1,text="Account").pack(side="left")
+        tk.Entry(master=self.frame1, textvariable=self.account).pack(side="left")
+        tk.Label(master=self.frame2, text="Password").pack(side="left")
+        tk.Entry(master=self.frame2, textvariable=self.password).pack(side="left")
+        tk.Button(master=self.frame3, text="Sign up").pack(side="left")
+        tk.Button(master=self.frame3, text="Log in").pack(side="left")
+        self.frame1.pack()
+        self.frame2.pack()
+        self.frame3.pack()
+
+class Sign_up_Frame(Base_Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.frame1 = tk.Frame(master)
+        self.frame2 = tk.Frame(master)
+        tk.Label(master=self.frame1, text="Choose your identity").pack()
+        tk.Button(master=self.frame2, text="Patient").pack(side="left")
+        tk.Button(master=self.frame2,text="Doctor").pack(side="left")
+        self.frame1.pack()
+        self.frame2.pack()
+
+
