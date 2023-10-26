b533d40ded4347cfae0680a3ff4e12567eafeddc
yangjun <1970520843@qq.com>
2023-10-26 17:25:51 +0800

    Sign_up

--- frame.py
+++ frame.py
@@ -7,15 +7,25 @@ class Base_Frame:
         self.tk_frame = ctk.CTkFrame(master)
         self.tk_frame.pack(fill="both",expand=True)
 
-    def Log_In(self):
+    def switch_Log_In(self):
         for widget in self.tk_frame.winfo_children():
             widget.destroy()
         Log_In_Frame(self.tk_frame)
 
-    def Sign_Up(self):
+    def switch_Sign_Up(self):
         for widget in self.tk_frame.winfo_children():
             widget.destroy()
         Sign_up_Frame(self.tk_frame)
+
+    def switch_Sign_Up_Patient(self):
+        for widget in self.tk_frame.winfo_children():
+            widget.destroy()
+        Sign_up_Patient_Frame(self.tk_frame)
+
+    def switch_Sign_Up_Doctor(self):
+        for widget in self.tk_frame.winfo_children():
+            widget.destroy()
+        Sign_up_Doctor_Frame(self.tk_frame)
 
 
 class Log_In_Frame(Base_Frame):
@@ -33,7 +43,7 @@ class Log_In_Frame(Base_Frame):
         ctk.CTkEntry(master=self.frame, textvariable = self.account, width=200).grid(row=0, column=1, columnspan=2, padx=10,pady=12)
         ctk.CTkEntry(master=self.frame, textvariable = self.password, width=200).grid(row=1, column=1, columnspan=2, padx=10,pady=12)
         ctk.CTkButton(master=self.frame, text="Log in", width=10).grid(row=2, column=1, padx=10,pady=12)
-        ctk.CTkButton(master=self.frame, text="Sign up", width=10, command=lambda: self.Sign_Up()).grid(row=2, column=2, padx=10,pady=12)
+        ctk.CTkButton(master=self.frame, text="Sign up", width=10, command=lambda: self.switch_Sign_Up()).grid(row=2, column=2, padx=10, pady=12)
         self.frame.pack()
 
 
@@ -54,9 +64,71 @@ class Sign_up_Frame(Base_Frame):
 
 
 
-        ctk.CTkLabel(master=self.frame, text="Choose your identity",width=200).grid(row=0, column=0, columnspan=2, padx=10,pady=12)
-        ctk.CTkButton(master=self.frame, text="Patient", width=10).grid(row=1, column=0, padx=10,pady=12)
-        ctk.CTkButton(master=self.frame, text="Doctor", width=10).grid(row=1, column=1, padx=10,pady=12)
-        ctk.CTkButton(master=self.frame, text="Back", width=5, command=lambda: self.Log_In()).grid(row=0, column=2, padx=10, pady=12)
+        ctk.CTkLabel(master=self.frame, text="Choose your identity",width=200).grid(
+            row=0, column=0, columnspan=2, padx=10,pady=12)
+        ctk.CTkButton(master=self.frame, text="Patient", width=10, command=lambda: self.switch_Sign_Up_Patient()).grid(
+            row=1, column=0, padx=10, pady=12)
+        ctk.CTkButton(master=self.frame, text="Doctor", width=10, command=lambda: self.switch_Sign_Up_Doctor()).grid(
+            row=1, column=1, padx=10,pady=12)
+        ctk.CTkButton(master=self.frame, text="Back", width=5, command=lambda: self.switch_Log_In()).grid(
+            row=0, column=2, padx=10, pady=12)
         self.frame.pack()
 
+class Sign_up_Patient_Frame(Base_Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.name = ctk.StringVar()
+        self.age = ctk.StringVar()
+        self.gender = ctk.StringVar()
+        self.contact_number = ctk.StringVar()
+        self.frame = self.frame = ctk.CTkFrame(self.tk_frame)
+
+        ctk.CTkLabel(master=self.frame, text="Please enter your message", width=200).grid(row=0, column=0, columnspan=3,
+                                                                                     padx=10, pady=12)
+        ctk.CTkLabel(master=self.frame, text='Name').grid(row=1, column=0, padx=10, pady=12)
+        ctk.CTkLabel(master=self.frame, text='Age').grid(row=2, column=0, padx=10, pady=12)
+        ctk.CTkLabel(master=self.frame, text='Gender').grid(row=3, column=0, padx=10, pady=12)
+        ctk.CTkLabel(master=self.frame, text='Contact number').grid(row=4, column=0, padx=10, pady=12)
+        ctk.CTkEntry(master=self.frame, textvariable=self.name, width=200).grid(row=1, column=1, columnspan=2,
+                                                                                   padx=10, pady=12)
+        ctk.CTkEntry(master=self.frame, textvariable=self.age, width=200).grid(row=2, column=1, columnspan=2,
+                                                                                padx=10, pady=12)
+        ctk.CTkEntry(master=self.frame, textvariable=self.gender, width=200).grid(row=3, column=1, columnspan=2,
+                                                                                padx=10, pady=12)
+        ctk.CTkEntry(master=self.frame, textvariable=self.contact_number, width=200).grid(row=4, column=1, columnspan=2,
+                                                                                padx=10, pady=12)
+        ctk.CTkButton(master=self.frame, text="Finish", width=5, command=lambda: self.switch_Log_In()).grid(row=5,
+                                                                                                          column=1,
+                                                                                                          padx=10,
+                                                                                                          pady=12)
+        self.frame.pack()
+
+
+class Sign_up_Doctor_Frame(Base_Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.name = ctk.StringVar()
+        self.age = ctk.StringVar()
+        self.gender = ctk.StringVar()
+        self.contact_number = ctk.StringVar()
+        self.frame = self.frame = ctk.CTkFrame(self.tk_frame)
+
+        ctk.CTkLabel(master=self.frame, text="Please enter your message", width=200).grid(row=0, column=0, columnspan=3,
+                                                                                     padx=10, pady=12)
+        ctk.CTkLabel(master=self.frame, text='Name').grid(row=1, column=0, padx=10, pady=12)
+        ctk.CTkLabel(master=self.frame, text='Age').grid(row=2, column=0, padx=10, pady=12)
+        ctk.CTkLabel(master=self.frame, text='Gender').grid(row=3, column=0, padx=10, pady=12)
+        ctk.CTkLabel(master=self.frame, text='Contact number').grid(row=4, column=0, padx=10, pady=12)
+        ctk.CTkEntry(master=self.frame, textvariable=self.name, width=200).grid(row=1, column=1, columnspan=2,
+                                                                                   padx=10, pady=12)
+        ctk.CTkEntry(master=self.frame, textvariable=self.age, width=200).grid(row=2, column=1, columnspan=2,
+                                                                                padx=10, pady=12)
+        ctk.CTkEntry(master=self.frame, textvariable=self.gender, width=200).grid(row=3, column=1, columnspan=2,
+                                                                                padx=10, pady=12)
+        ctk.CTkEntry(master=self.frame, textvariable=self.contact_number, width=200).grid(row=4, column=1, columnspan=2,
+                                                                                padx=10, pady=12)
+        ctk.CTkButton(master=self.frame, text="Finish", width=5, command=lambda: self.switch_Log_In()).grid(row=5,
+                                                                                                          column=1,
+                                                                                                          padx=10,
+                                                                                                          pady=12)
+        self.frame.pack()
