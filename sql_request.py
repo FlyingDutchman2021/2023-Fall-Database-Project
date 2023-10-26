39050ba97668e5cf7ef2a7adbdf98b1b6c71d97e
FlyingDutchman2021 <89907122+FlyingDutchman2021@users.noreply.github.com>
2023-10-26 22:24:14 +0800

    third try

--- sql_request.py
+++ sql_request.py
@@ -1,12 +1,24 @@
 import sqlite3
 
 
-def sql_request(db: str, sql: str):
+def strip_sql(entry: str):
+    entry = entry.strip()
+    return entry
+
+
+def validating(entry: str):
+    for char in entry:
+        if char == ' ':
+            return False
+    return True
+
+
+def sql_request(SQL: str, db: str = 'hospital_system.db'):
     try:
         with sqlite3.connect(database=db) as db:
             temp_cursor = db.cursor()
-            print(sql)
-            temp_cursor.execute(sql)
+            print(SQL)
+            temp_cursor.execute(SQL)
             temp_result = temp_cursor.fetchall()
             temp_cursor.close()
             return 'Success', temp_result
@@ -14,3 +26,11 @@ def sql_request(db: str, sql: str):
     except sqlite3.Error:
         print(sqlite3.Error)
         return sqlite3.Error, ''
+
+
+# sql = '''SELECT * FROM pending_doctor_info'''
+# result = sql_request(sql)
+# print(result)
+s = 'shfdkalj  '
+print(validating(strip_sql(s)))
+
