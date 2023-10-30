d312fee65bcc5fea7445600d93894835abff8cfd
FlyingDutchman2021 <89907122+FlyingDutchman2021@users.noreply.github.com>
2023-10-30 21:58:43 +0800

    write some sql codes

--- sql_request.py
+++ sql_request.py
@@ -1,11 +1,13 @@
 import sqlite3
 
 
+#Tested
 def strip_sql(entry: str):
     entry = entry.strip()
     return entry
 
 
+# Tested
 def validating(entry: str):
     for char in entry:
         if char == ' ':
@@ -13,6 +15,7 @@ def validating(entry: str):
     return True
 
 
+# Tested
 def sql_request(SQL: str, db: str = 'hospital_system.db'):
     try:
         with sqlite3.connect(database=db) as db:
@@ -28,9 +31,32 @@ def sql_request(SQL: str, db: str = 'hospital_system.db'):
         return sqlite3.Error, ''
 
 
+# Tested
+# Find password according to id
+def find_password(_id: int):
+    sql = '''SELECT password From password WHERE id = %d''' % _id
+    return sql_request(sql)
+
+
+def find_patient_info(_id: int):
+    sql = '''SELECT * FROM patient_info WHERE id = %d''' % _id
+    return sql_request(sql)
+
 # sql = '''SELECT * FROM pending_doctor_info'''
+
+
+# id = 10001
+# password = 'sakhjf;jaskdjf;j'
+# sql = '''INSERT INTO password (id, password) VALUES (%d, '%s')''' % (id, password)
+
+
+# id = 10001
+# sql = '''SELECT * From password WHERE id = %d''' % id
+
+# sql = '''SELECT * From pending_doctor_info'''
+
+# table = 'password'
+# sql = '''SELECT * From %s''' % table
 # result = sql_request(sql)
 # print(result)
-s = 'shfdkalj  '
-print(validating(strip_sql(s)))
 
