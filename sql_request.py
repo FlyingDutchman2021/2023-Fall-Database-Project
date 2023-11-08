93861f7875ddf9a2d4f2047d6a81f968212fd918
FlyingDutchman2021 <89907122+FlyingDutchman2021@users.noreply.github.com>
2023-11-08 23:54:50 +0800

    Continue doing the backend SQL codes Warp the internal functions with public functions

--- sql_request.py
+++ sql_request.py
@@ -1,46 +1,105 @@
 import sqlite3
 
 
-#Tested
-def strip_sql(entry: str):
-    entry = entry.strip()
-    return entry
+# # Input a string, strip the blanks front and back
+# def _strip_sql(entry: str):
+#     entry = entry.strip()
+#     return entry
 
 
-# Tested
-def validating(entry: str):
+# Validate an entry value. If a string contains any blank space, return False
+#
+def _isValid(entry: str):
     for char in entry:
         if char == ' ':
             return False
     return True
 
 
-# Tested
-def sql_request(SQL: str, db: str = 'hospital_system.db'):
+# Base sql request framework
+# Return <status>, <result list>
+# <status>:
+# 'Success': The SQL is executed successfully
+# <error>: The SQL can not be executed due to some database level error
+#
+# TODO dev_mode_on
+def _sql_request(SQL: str, db: str = 'hospital_system.db', dev_mode_on=True):
     try:
         with sqlite3.connect(database=db) as db:
             temp_cursor = db.cursor()
-            print(SQL)
+            if dev_mode_on:
+                print(SQL)
             temp_cursor.execute(SQL)
             temp_result = temp_cursor.fetchall()
             temp_cursor.close()
             return 'Success', temp_result
 
     except sqlite3.Error:
-        print(sqlite3.Error)
-        return sqlite3.Error, ''
+        return sqlite3.Error, []
+
+
+# Find password according to id and type
+# Only return password
+# @para _id: 12 digits integer,
+# _type: string either be 'P', 'D' or 'N'
+#
+def _find_password_id(_id: int, _type: str):
+    sql = "SELECT password From password WHERE id = %d AND type = '%s'" % (_id, _type)
+    return _sql_request(sql)
 
 
 # Tested
-# Find password according to id
-def find_password(_id: int):
-    sql = '''SELECT password From password WHERE id = %d''' % _id
-    return sql_request(sql)
-
-
-def find_patient_info(_id: int):
+# Find patient info using id
+# Return email, name, sex, birthdate, blood type, contact number, and notes
+def _find_patient_info(_id: int):
     sql = '''SELECT * FROM patient_info WHERE id = %d''' % _id
-    return sql_request(sql)
+    return _sql_request(sql)
+
+#
+#
+# Public functions
+#
+#
+
+
+# Return <status>
+# 'Success': successfully log in to the system
+# 'User not found': no such user exists
+# 'Wrong Password': password is wrong
+# 'Illegal username': username is illegal
+# 'Invalid username': username is not any one of id, contact number or email
+#
+def login(_id_phone_email: str, _password: str, _identity: str):
+    _id_phone_email = _id_phone_email.strip()
+    _password = _password.strip()
+    if not _isValid(_id_phone_email):
+        return 'Illegal username'
+
+    if _id_phone_email.isdigit():
+        if len(_id_phone_email) == 11:
+            phone_number = int(_id_phone_email)
+            # TODO Phone Number search
+        elif len(_id_phone_email) == 12:
+            id = int(_id_phone_email)
+            # TODO id search
+        else:
+            return 'Invalid username'
+    elif '@' in _id_phone_email:
+        email = _id_phone_email
+        # TODO email search
+    else:
+        return 'Invalid username'
+
+
+print(0)
+
+
+
+
+
+
+
+
 
 # sql = '''SELECT * FROM pending_doctor_info'''
 
